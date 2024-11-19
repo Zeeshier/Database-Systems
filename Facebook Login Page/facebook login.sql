cf87cfdf1410c56101c441abc3d9d35b05c8cddb
Zeeshan Ahmad <Zeeshanwarraich51@gmail.com>
2024-11-19 23:14:05 +0500

    Facebook login

---
+++ Facebook Login Page/facebook login.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE facebook_login;
+
+USE facebook_login;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
