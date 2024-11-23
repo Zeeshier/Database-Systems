8afb5ec49859158d979e859fe26c8980a16efb8f
Zeeshan Ahmad <Zeeshanwarraich51@gmail.com>
2024-11-23 23:10:09 +0500

    Clauses

---
+++ 03. SQL Clauses.sql
@@ -0,0 +1,15 @@
+-- With Clause
+WITH NewStudents AS (
+    SELECT 7 AS id, 'Ali' AS name, 19 AS age, 'Computer Science' AS department
+    UNION ALL
+    SELECT 8, 'Babar', 18, 'Pharmacy'
+    UNION ALL
+    SELECT 9, 'Junaid', 19, 'Biology'
+    UNION ALL
+    SELECT 10, 'Hashir', 20, 'LBS'
+    UNION ALL
+    SELECT 11, 'Furkan', 21, 'English'
+)
+INSERT INTO students (id, name, age, department)
+SELECT id, name, age, department
+FROM NewStudents;
